ad2d33fbd658cdcce61290ffe7e6c933dc4899d0
Cristian Dylan D'Amico <cristian.damico.08@marconigorgonzola.it>
2026-04-18 16:34:14 +0200

    Aggiunti appunti sui tuple

---
+++ StudioCasa/Tuple.py
@@ -0,0 +1,9 @@
+# I tuple sono come gli array statici di Java
+
+# I comandi sono uguali a quelli delle liste solo che, una volta creati, non possono essere modificati
+# A differenza delle liste, i tuple si dichiarano con le parentesi tonde
+
+# Per riconoscere se una variabile è tuple si può usare il comando type()
+tuple1 = ("mela", "arancia", "banana")
+
+print(type(tuple1))
