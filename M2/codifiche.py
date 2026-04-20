8c81160d79c0024a605bb3f16e22b2d7f0a71684
Cristian Dylan D'Amico <cristian.damico.08@marconigorgonzola.it>
2026-04-20 17:00:43 +0200

    aggiunti primi appunti per il modulo M2

---
+++ M2/codifiche.py
@@ -0,0 +1,7 @@
+print(ord("A"))     # Serve a stampare il codepoint del carattere
+print(chr(65))      # Serve a stampare il carattere del codepoint
+
+stringaMultilinea = """Oggi e' una bella giornata di sole 
+e noi siamo qui a studiare Python!!!"""    # Posso utilizzare 3 " ed andare a capo quando voglio al posto del \n
+
+print(stringaMultilinea)
