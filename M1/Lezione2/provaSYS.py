0214fb041f8a45a091a782a9d09a998565c95769
Cristian Dylan D'Amico <cristian.damico.08@marconigorgonzola.it>
2026-04-20 15:26:05 +0200

    Spostamento file nella cartella M1

---
+++ M1/Lezione2/provaSYS.py
@@ -0,0 +1,5 @@
+import sys
+
+for p in sys.path:    # sys.path stampa il percorso dove è posizionato il file, dove sono memorizzati i moduli di python e dove sono salvate le librerie
+    print(p)
+
